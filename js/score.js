9a754b8df3273f15b74323ae0cd978fc55c69874
rpashyk <rpashyk@gmail.com>
2024-06-12 23:37:51 +0300

    Add files via upload

--- js/score.js
+++ js/score.js
@@ -1,12 +1,13 @@
-document.getElementById('plus1').addEventListener("click", onClickPlus);
-document.getElementById('minus1').addEventListener("click", onClickMinus);
-document.getElementById('death1').addEventListener("click", onClickDeath);
-document.getElementById('name1').addEventListener("dblclick", onDblClickName);
-document.getElementById('name1').addEventListener("touchend", onDblClickName);
-document.getElementById('delete1').addEventListener("click", onClickDelete);
-document.getElementById('addPlayer').addEventListener("click", onClickAddPlayer);
+$('.plus').click(onClickPlus);
+$('.minus').click(onClickMinus);
+$('.death').click(onClickDeath);
+$('.name').dblclick(onDblClickName);
+// $('.name').touchend(onDblClickName);
+$('.delete').click(onClickDelete);
+$('.color').click(onClickAddPlayer);
 
-var countPlayer = 1;
+//document.getElementById('addPlayer').addEventListener("click", onClickAddPlayer);
+
 
 function onClickPlus(){
     var num = this.id[4];
@@ -41,70 +42,47 @@ function onClickDeath(){
 }
 
 function onClickAddPlayer(){
-    countPlayer++;
-    if(countPlayer > 6) {
-        alert("maximum players is 6");
-        countPlayer--;
-        return;
+    var num = 0;
+
+    switch(this.id) {
+        case 'addyellow': num = 1; break;
+        case 'addred': num = 2; break;
+        case 'addblue': num = 3; break;
+        case 'addgreen': num = 4; break;
+        case 'addpurple': num = 5; break;
+        case 'addorange': num = 6; break;
     }
+    $('main #score #player' + num).show();
+    $('main .map #' + this.id.slice(3, this.id.length + 1) + '1').show();
 
-    var output = document.getElementById('score');
-
-    let newbutton0 = document.createElement('button');
-    newbutton0.classList.add('death'); 
-    newbutton0.id = 'death' + countPlayer;
-    newbutton0.textContent = 'Dead';
-
-    let newbutton1 = document.createElement('button');
-    newbutton1.id = 'plus' + countPlayer;
-    newbutton1.textContent = '+';
-
-    let counter = document.createElement('h6');
-    counter.id = 'count' + countPlayer;
-    counter.textContent = 0;
-
-    let newbutton2 = document.createElement('button');
-    newbutton2.id = 'minus' + countPlayer;
-    newbutton2.textContent = '-';
-
-    let name = document.createElement('h6');
-    name.classList.add('name'); 
-    name.id = 'name' + countPlayer;
-    name.textContent = 'Name' + countPlayer;
-
-    let newbutton3 = document.createElement('button');
-    newbutton3.id = 'delete' + countPlayer;
-    newbutton3.textContent = 'x';
-
-    let newMessage = document.createElement('div'); 
-    newMessage.id = 'player' + countPlayer;
-    newMessage.classList.add('player'); 
-    newMessage.appendChild(newbutton0);
-    newMessage.appendChild(newbutton1);
-    newMessage.appendChild(counter);
-    newMessage.appendChild(newbutton2);
-    newMessage.appendChild(name); 
-    newMessage.appendChild(newbutton3);
-    output.appendChild(newMessage);
-
-    document.getElementById('plus' + countPlayer).addEventListener("click", onClickPlus);
-    document.getElementById('minus' + countPlayer).addEventListener("click", onClickMinus);
-    document.getElementById('death' + countPlayer).addEventListener("click", onClickDeath);
-    document.getElementById('name' + countPlayer).addEventListener("dblclick", onDblClickName);
-    document.getElementById('name' + countPlayer).addEventListener("touchend", onDblClickName);
-    document.getElementById('delete' + countPlayer).addEventListener("click", onClickDelete);
 }
 
 function onClickDelete(){
-    var num = this.id[6];
-    if(num == countPlayer){
-        countPlayer--;
-        var parent = document.getElementById('score');
-        var child = document.getElementById('player' + num);
-        parent.removeChild(child);
+    $('main #score #player' + this.id[6]).hide();
+    var color;
+    switch(this.id[6]) {
+        case '1': color = 'yellow'; break;
+        case '2': color = 'red'; break;
+        case '3': color = 'blue'; break;
+        case '4': color = 'green'; break;
+        case '5': color = 'purple'; break;
+        case '6': color = 'orange'; break;
     }
-    else{
-        alert("Начинайте удаление с последнего игрока");
-    }
-    
+    $('main .map img[id^='+ color +']').css('left', '').css('top', '').hide();
 }
+
+$('main .map img').hide();
+$('#score .player ').hide();
+$(function() {
+            
+    $('.dragElement').draggable({
+        containment: ".map"
+    });
+    
+});
+$('.map div').css('background-image', function(){
+    return 'url("images/' + this.id + '.jpg")'
+});
+$('.map div').css('background-size', 'contain');
+
+//style="background-image: url('images/lvl1.jpg'); background-size: contain;"
